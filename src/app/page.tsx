6277e481e7aff43586cc313db82560e7843158b3
NicolasBonnefont <nicolas.7l@hotmail.com>
2024-02-02 15:56:33 -0300

    ajustado datas dos eventos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,20 +25,20 @@ export default function MyCalendar() {
   const events = [
     {
       title: 'Evento 1',
-      start: new Date(2024, 1, 2, 8, 0),
-      end: new Date(2024, 1, 2, 9, 0),
+      start: addHours((startOfDay(new Date())), 8),
+      end: addHours((startOfDay(new Date())), 9),
       resourceId: 'sala1',
     },
     {
       title: 'Evento 2',
-      start: new Date(2024, 1, 2, 10, 0),
-      end: new Date(2024, 1, 2, 11, 0),
+      start: addHours((startOfDay(new Date())), 9),
+      end: addHours((startOfDay(new Date())), 10),
       resourceId: 'sala2',
     },
     {
       title: 'Evento 3',
-      start: new Date(2024, 1, 2, 12, 0),
-      end: new Date(2024, 1, 2, 13, 0),
+      start: addHours((startOfDay(new Date())), 10),
+      end: addHours((startOfDay(new Date())), 11),
       resourceId: 'sala3',
     },
   ];
